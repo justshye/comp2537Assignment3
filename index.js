052a0493a0f78a74e241dc55098416abf54b9279
shye <113232835+justshye@users.noreply.github.com>
2023-05-10 11:00:31 -0700

    creates modal to display pokemon's details and front sprite

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const setup = async () => {
     //test out poke api using axios here
-    
+
     $('#pokeCards').empty()
     let response = await axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=810');
     const pokemons = response.data.results;
     pokemons.forEach(async (pokemon) => {
         const res = await axios.get(pokemon.url)
         $('#pokeCards').append(`
-        ${res.data.name.toUpperCase()}
+        <div class=pokeCard card">
+            <h3>${res.data.name.toUpperCase()}</h3>
+            <img src="${res.data.sprites.front_default}" alt="${res.data.name}"/>
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#pokeModal">More</button>
+        </div>
         `)
     })
 }
