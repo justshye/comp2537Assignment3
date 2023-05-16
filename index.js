db5352346e351248bba8412e7eebbabe10d85e88
shye <113232835+justshye@users.noreply.github.com>
2023-05-15 18:42:31 -0700

    adds pokemonCount to index.html

--- index.js
+++ index.js
@@ -5,61 +5,84 @@ let pokemons = [];
 const updatePaginationDiv = (currentPage, numPages) => {
   $('#pagination').empty();
 
-  const startPage = Math.max(1, currentPage - 4);
-  const endPage = Math.min(numPages, startPage + 4);
+  let startPage;
+  let endPage;
+
+  if (numPages <= 5) {
+      startPage = 1;
+      endPage = numPages;
+  } else if (currentPage <= 3) {
+      startPage = 1;
+      endPage = 5;
+  } else if (currentPage >= numPages - 2) {
+      startPage = numPages - 4;
+      endPage = numPages;
+  } else {
+      startPage = currentPage - 2;
+      endPage = currentPage + 2;
+  }
 
   for (let i = startPage; i <= endPage; i++) {
-    $('#pagination').append(`
-      <button class="btn btn-primary page ml-1 numberedButtons ${i === currentPage ? 'active' : ''}" value="${i}">${i}</button>
-    `);
+      $('#pagination').append(`
+          <button class="btn btn-primary page ml-1 numberedButtons ${i === currentPage ? 'active' : ''}" value="${i}">${i}</button>
+      `);
   }
 
   const prevButton = `
-    <button class="btn btn-primary page ml-1 prevButton ${currentPage === 1 ? 'd-none' : ''}">Previous</button>
+      <button class="btn btn-primary page ml-1 prevButton ${currentPage === 1 ? 'd-none' : ''}">Previous</button>
   `;
   const nextButton = `
-    <button class="btn btn-primary page ml-1 nextButton ${currentPage === numPages ? 'd-none' : ''}">Next</button>
+      <button class="btn btn-primary page ml-1 nextButton ${currentPage === numPages ? 'd-none' : ''}">Next</button>
   `;
 
   $('#pagination').prepend(prevButton);
   $('#pagination').append(nextButton);
+
+  const totalPokemonCount = pokemons.length;
+  const startRange = (currentPage - 1) * PAGE_SIZE + 1;
+  const endRange = Math.min(currentPage * PAGE_SIZE, totalPokemonCount);
+  $('#pokemonCount').text(`Showing ${startRange} - ${endRange} of ${totalPokemonCount} Pokémon`);
 };
+
 
 const paginate = async (currentPage, PAGE_SIZE, pokemons) => {
-  const start = (currentPage - 1) * PAGE_SIZE;
-  const end = start + PAGE_SIZE;
-  const selected_pokemons = pokemons.slice(start, end);
+    const start = (currentPage - 1) * PAGE_SIZE;
+    const end = start + PAGE_SIZE;
+    const selected_pokemons = pokemons.slice(start, end);
 
-  $('#pokeCards').empty();
-  for (const pokemon of selected_pokemons) {
-    const res = await axios.get(pokemon.url);
-    $('#pokeCards').append(`
-      <div class="pokeCard card" pokeName="${res.data.name}">
-        <h3>${res.data.name.toUpperCase()}</h3>
-        <img src="${res.data.sprites.front_default}" alt="${res.data.name}"/>
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#pokeModal">More</button>
-      </div>
-    `);
-  }
+    $('#pokeCards').empty();
+    for (const pokemon of selected_pokemons) {
+        const res = await axios.get(pokemon.url);
+        $('#pokeCards').append(`
+        <div class="pokeCard card" pokeName="${res.data.name}">
+          <h3>${res.data.name.toUpperCase()}</h3>
+          <img src="${res.data.sprites.front_default}" alt="${res.data.name}"/>
+          <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#pokeModal">More</button>
+        </div>
+      `);
+    }
+
+    const totalPokemonCount = pokemons.length;
+    const startRange = (currentPage - 1) * PAGE_SIZE + 1;
+    const endRange = Math.min(currentPage * PAGE_SIZE, totalPokemonCount);
+    $('#displayedPokemonCount').text(`Showing ${startRange} - ${endRange} of ${totalPokemonCount} Pokémon`);
 };
 
+
 const setup = async () => {
-  $('#pokeCards').empty();
-  let response = await axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=810');
-  pokemons = response.data.results;
+    $('#pokeCards').empty();
+    let response = await axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=810');
+    pokemons = response.data.results;
 
-  const numPages = Math.ceil(pokemons.length / PAGE_SIZE);
-  paginate(currentPage, PAGE_SIZE, pokemons);
-  updatePaginationDiv(currentPage, numPages);
+    const numPages = Math.ceil(pokemons.length / PAGE_SIZE);
+    paginate(currentPage, PAGE_SIZE, pokemons);
+    updatePaginationDiv(currentPage, numPages);
 
-  $('#pokemonCount').text(`Total Pokemon: ${pokemons.length}`);
-  $('#displayedPokemonCount').text(`Displayed Pokemon: ${Math.min(PAGE_SIZE, pokemons.length)}`);
-
-  $('body').on('click', '.pokeCard', async function (e) {
-    const pokemonName = $(this).attr('pokeName');
-    const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
-    const types = res.data.types.map((type) => type.type.name);
-    $('.modal-body').html(`
+    $('body').on('click', '.pokeCard', async function (e) {
+        const pokemonName = $(this).attr('pokeName');
+        const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+        const types = res.data.types.map((type) => type.type.name);
+        $('.modal-body').html(`
       <div style="width:200px">
         <img src="${res.data.sprites.other['official-artwork'].front_default}" alt="${res.data.name}"/>
         <div>
@@ -80,38 +103,38 @@ const setup = async () => {
         </ul>
       </div>
     `);
-    $('.modal-title').html(`
+        $('.modal-title').html(`
       <h2>${res.data.name.toUpperCase()}</h2>
       <h5>${res.data.id}</h5>
     `);
-  });
-  
-  // add event listener to pagination buttons
-  $('body').on('click', '.numberedButtons', async function (e) {
-    currentPage = Number(e.target.value);
-    paginate(currentPage, PAGE_SIZE, pokemons);
-    updatePaginationDiv(currentPage, numPages);
-  });
-  
-  // add event listener to previous button
-  $('body').on('click', '.prevButton', async function (e) {
-    if (currentPage > 1) {
-      currentPage--;
-      paginate(currentPage, PAGE_SIZE, pokemons);
-      updatePaginationDiv(currentPage, numPages);
-    }
-  });
-  
-  // add event listener to next button
-  $('body').on('click', '.nextButton', async function (e) {
-    if (currentPage < numPages) {
-      currentPage++;
-      paginate(currentPage, PAGE_SIZE, pokemons);
-      updatePaginationDiv(currentPage, numPages);
-    }
-  });
-  
-  };
-  
-  $(document).ready(setup);
-  
+    });
+
+    // add event listener to pagination buttons
+    $('body').on('click', '.numberedButtons', async function (e) {
+        currentPage = Number(e.target.value);
+        paginate(currentPage, PAGE_SIZE, pokemons);
+        updatePaginationDiv(currentPage, numPages);
+    });
+
+    // add event listener to previous button
+    $('body').on('click', '.prevButton', async function (e) {
+        if (currentPage > 1) {
+            currentPage--;
+            paginate(currentPage, PAGE_SIZE, pokemons);
+            updatePaginationDiv(currentPage, numPages);
+        }
+    });
+
+    // add event listener to next button
+    $('body').on('click', '.nextButton', async function (e) {
+        if (currentPage < numPages) {
+            currentPage++;
+            paginate(currentPage, PAGE_SIZE, pokemons);
+            updatePaginationDiv(currentPage, numPages);
+        }
+    });
+
+};
+
+$(document).ready(setup);
+
